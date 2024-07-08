67668cee20f78066e18cab76141abb2bc751ba13
TamilselvanSrmist <tamilsem3@srmist.edu.in>
2024-07-08 14:07:00 +0530

    project backend setup

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials:true
+}));
+
+
+module.exports = app;
